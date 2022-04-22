5984d6affcfd65dfdde04719faf178cda761b62f
IgnacioRodriguez98 <99094740+IgnacioRodriguez98@users.noreply.github.com>
2022-04-21 22:59:21 -0600

    as

--- MYAPP.py
+++ MYAPP.py
@@ -32,7 +32,7 @@ else:
     df = pd.read_csv('https://raw.githubusercontent.com/IgnacioRodriguez98/Monitoreo-TR/main/Data/normJA.csv', header=None)
     st.write(df)
 
-database2 = database["0"]
+database2 = database[,1]
 
 if C2H2 == True and (H2,C2H4,CO,C2H6,CH4) == False:
     database2 = database2.append(database[1]) 
