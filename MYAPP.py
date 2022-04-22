56b8394c3f439105b570473a21aafcc1c42661a3
IgnacioRodriguez98 <99094740+IgnacioRodriguez98@users.noreply.github.com>
2022-04-21 22:49:45 -0600

    a

--- MYAPP.py
+++ MYAPP.py
@@ -26,12 +26,12 @@ elif database == '2':
     st.write("""# Has seleccionado la base de datos 2""")
     df = pd.read_csv('https://raw.githubusercontent.com/IgnacioRodriguez98/Monitoreo-TR/main/Data/normCH.csv', header=None)
     st.write(df)
-
+"
 else:
     st.write("""# Has seleccionado la base de datos 3""")
     df = pd.read_csv('https://raw.githubusercontent.com/IgnacioRodriguez98/Monitoreo-TR/main/Data/normJA.csv', header=None)
     st.write(df)
-database2 = database[1]
+database2 = database["1"]
 if C2H2 == True and (H2,C2H4,CO,C2H6,CH4) == False:
     database2 = database2.append(database[1]) 
 elif C2H2 == True and H2 == True:
