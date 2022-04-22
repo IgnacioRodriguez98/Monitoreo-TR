5c632cacaaf6102764267512f8449fa5e4800f63
IgnacioRodriguez98 <99094740+IgnacioRodriguez98@users.noreply.github.com>
2022-04-21 22:31:26 -0600

    a

--- MYAPP.py
+++ MYAPP.py
@@ -16,7 +16,7 @@ with st.sidebar:
     C2H6 = st.checkbox("Etano")
     CH4 = st.checkbox("Metano")
 
-if C2H2 == check and (H2,C2H4,CO,C2H6,CH4) != check:
+if C2H2 == True and (H2,C2H4,CO,C2H6,CH4) == False:
     database = database["0"]
 elif C2H2 == check and H2 == check:
     database = database["0","1"]
