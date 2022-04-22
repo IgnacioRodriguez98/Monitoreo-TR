232fcc29d0667428bba5ca8785133ef0732e7b94
IgnacioRodriguez98 <99094740+IgnacioRodriguez98@users.noreply.github.com>
2022-04-21 23:39:41 -0600

    asd

--- MYAPP.py
+++ MYAPP.py
@@ -32,9 +32,6 @@ else:
     df = pd.read_csv('https://raw.githubusercontent.com/IgnacioRodriguez98/Monitoreo-TR/main/Data/normJA.csv', header=None)
     st.write(df)
 
-lst = []
-p= df[0]
-
 if C2H2:
     a = df[[0,1]]
     lst.append(a)
@@ -61,6 +58,9 @@ elif CH4:
 
 else:
     p="""### No hay gases seleccionados, por favor selecciona al menos uno"""
+
+lst = []
+p= df[0]
 
 if  len(lst) > 0:
     for i in lst:
