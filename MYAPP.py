e72c5e03bd1c4b9673c09a9ed079d2f29566304b
Ignacio Rodriguez Sibaja <99094740+IgnacioRodriguez98@users.noreply.github.com>
2022-05-02 17:45:28 -0600

    asd

--- MYAPP.py
+++ MYAPP.py
@@ -5,6 +5,8 @@ import pandas as pd
 import time
 import numpy as np
 from github import Github
+import streamlit.components.v1 as components
+import matplotlib.animation as animation
 
 
 with st.sidebar:
@@ -110,3 +112,31 @@ if st.button('Cargar a Github'):
     else:
         repo.create_file(git_file, "committing files", content, branch="master")
         print(git_file + ' CREATED')
+
+if st.button('Prueba de Grafica'):
+    def func(t, line):
+        t = np.arange(0,t,0.1)
+        y = np.sin(t)
+        line.set_data(t, y)
+        return line
+    
+    fig = plt.figure()
+    ax = plt.axes(xlim=(0, 100), ylim=(-1.2, 1.22))
+    redDots = plt.plot([], [], 'ro')
+    line = plt.plot([], [], lw=2)
+    
+
+    # Creating the Animation object
+    line_ani = animation.FuncAnimation(fig, func, frames=np.arange(1,100,0.1), fargs=(line), interval=100, blit=False)
+    #line_ani.save(r'Animation.mp4')
+    
+    
+
+    #HtmlFile = line_ani.to_html5_video()
+    with open("myvideo.html","w") as f:
+    print(line_ani.to_html5_video(), file=f)
+    
+    HtmlFile = open("myvideo.html", "r")
+    #HtmlFile="myvideo.html"
+    source_code = HtmlFile.read() 
+    components.html(source_code, height = 900,width=900)
