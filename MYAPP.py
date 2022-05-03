c1455e2c32ba0727fc27f8e023c650085c19eeed
Ignacio Rodriguez Sibaja <99094740+IgnacioRodriguez98@users.noreply.github.com>
2022-05-03 17:51:07 -0600

    asd

--- MYAPP.py
+++ MYAPP.py
@@ -109,35 +109,34 @@ va = []
 nm = []
 c = 0
 gr = pd.DataFrame()
-st.write(header[1:])
 for i in range(len(q)):
-    for k in range(len(lista)+1):
+    for k in len(header[1:]):
         dat.append(q["Date"][i])
-    for k in header:
-        va.append(q[k][i])
     #dat.append(q["Date"][i])
     #dat.append(q["Date"][i])
     #dat.append(q["Date"][i])
     #dat.append(q["Date"][i])
     #dat.append(q["Date"][i])
     #dat.append(q["Date"][i])
-    #va.append(q["Acetileno"][i])
-    #va.append(q["Hidrogeno"][i])
-    #va.append(q["Etileno"][i])
-    #va.append(q["Monoxido de carbono"][i])
-    #va.append(q["Etano"][i])
-    #va.append(q["Metano"][i])
-    #for k in header[1:]:
-    #    gs.append(k)
-    #gs.append(header[1:])
-    #gs.append("Anomalia")
+    va.append(q["Acetileno"][i])
+    va.append(q["Hidrogeno"][i])
+    va.append(q["Etileno"][i])
+    va.append(q["Monoxido de carbono"][i])
+    va.append(q["Etano"][i])
+    va.append(q["Metano"][i])
+    gs.append("Acetileno")
+    gs.append("Hidrogeno")
+    gs.append("Etileno")
+    gs.append("Monoxido de carbono")
+    gs.append("Etano")
+    gs.append("Metano")
 
-#for i in range(len(gs)):
-#    nm.append(i)
+for i in range(len(gs)):
+    nm.append(i)
 
-#gr["Num"]= nm
+gr["Num"]= nm
 gr["Date"]=dat
-#gr["Gas"]= gs
+gr["Gas"]= gs
 gr["Valor"]= va
 
 
