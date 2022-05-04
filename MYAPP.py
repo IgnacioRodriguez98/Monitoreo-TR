53fdf08cd9ee45bbfb47f0628dfe694fa88a7800
Ignacio Rodriguez Sibaja <99094740+IgnacioRodriguez98@users.noreply.github.com>
2022-05-03 18:34:57 -0600

    asd

--- MYAPP.py
+++ MYAPP.py
@@ -94,7 +94,9 @@ if len(lista)> 0:
 if C2H2== False |H2 == False | C2H4 == False |CO == False |C2H6 ==False |CH4 == False:
    p="""### No hay gases seleccionados, por favor selecciona al menos uno"""
 
-if len(header) > 1:
+if len(header) == 1:
+    st.write("""### No hay gases seleccionados, por favor selecciona al menos uno""")
+else:    
     q = p
     q.columns = header
     q["Date"] = pd.to_datetime(q["Date"]).dt.strftime("%Y-%m-%d %H:%M:%S")
