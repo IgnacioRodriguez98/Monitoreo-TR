de6c49bf10c1d9905a7d4aec67241b2f9f6de24a
Ignacio Rodriguez Sibaja <99094740+IgnacioRodriguez98@users.noreply.github.com>
2022-05-06 02:02:44 -0600

    asd

--- MYAPP.py
+++ MYAPP.py
@@ -131,7 +131,7 @@ if len(lista)> 0:
     q.columns = header
     q["Date"] = pd.to_datetime(q["Date"]).dt.strftime("%Y-%m-%d %H:%M:%S")
 
-if st.button("Comenzar"):
+if st.markdown("Comenzar"):
     st.write("""# Modelos de Machine Learning
     Para la logra la detección de anomalias en esta app se hace uso de dos modelos de Machine Learning,
     **Autoencoder** para cuando se desea detectar anomalias en 2 o más gases y 
